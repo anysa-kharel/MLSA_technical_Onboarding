de938e87d0255eb9458b363dcd234c7ed7df13f7
anysa-kharel <anishakharel456@gmail.com>
2023-10-16 11:46:52 +0545

    add all changes

--- src/App.jsx
+++ src/App.jsx
@@ -24,15 +24,15 @@ import "./styles.css";
  * If you don't have one of the social sites listed, leave it as an empty string.
  */
 const siteProps = {
-  name: "Alexandrie Grenier",
-  title: "Web Designer & Content Creator",
-  email: "alex@example.com",
-  gitHub: "microsoft",
-  instagram: "microsoft",
-  linkedIn: "satyanadella",
+  name: "Anisha Kharel",
+  title: "Visual Designer | UI/UX Fanatic | Front-End Dev: Blending Art and Innovation!",
+  email: "ansukharel456@gmail.com",
+  gitHub: "anysa-kharel",
+  instagram: "",
+  linkedIn: "anisha-kharel-502207244/",
   medium: "",
-  twitter: "microsoft",
-  youTube: "Code",
+  twitter: "",
+  youTube: "",
 };
 
 const primaryColor = "#4E567E";
--- src/Components/Portfolio.jsx
+++ src/Components/Portfolio.jsx
@@ -19,9 +19,9 @@ import React from "react";
  * Need an image? Check out https://unsplash.com to download a photo you
  * freely use on your site.
  */
-import image from "../images/design-desk.jpeg";
+import image from "../images/portfolio-bg.jpg";
 
-const imageAltText = "desktop with books and laptop";
+const imageAltText = "Background image";
 
 /**
  * Project list
@@ -31,28 +31,22 @@ const imageAltText = "desktop with books and laptop";
  */
 const projectList = [
   {
-    title: "10 Things To Know About Azure Static Web Apps 🎉",
+    title: "University Management System",
     description:
-      "Collaboration to create a beginner friendly article to help explain Azure Static Web Apps and tooling to get started.",
-    url: "https://dev.to/azure/10-things-to-know-about-azure-static-web-apps-3n4i",
+      "It is a University Management System developed using React,Dango and SQL lite.",
+    url: "https://github.com/anysa-kharel/University-Management-System",
   },
   {
-    title: "Web Development for Beginners",
+    title: "E-Commerce Site",
     description:
-      "Contributed sketch note imagery to accompany each lesson. These help provide visual representation of what is being taught.",
-    url: "https://github.com/microsoft/web-dev-for-beginners",
+      "It is e-commerce website developed using ReactJS.",
+    url: "https://lets-shop-online.netlify.app/",
   },
   {
-    title: "My Resume Site",
+    title: "Volunteer Management System",
     description:
-      "Created from Microsoft's resume workshop and deployed to GitHub pages. Includes my experience and design abilities.",
-    url: "https://github.com/microsoft/workshop-library/tree/main/full/build-resume-website",
-  },
-  {
-    title: "GitHub Codespaces and github.dev",
-    description:
-      "Video interview to explain when to use GitHub.dev versus GitHub Codespaces, and how best to use each tool.",
-    url: "https://www.youtube.com/watch?v=c3hHhRME_XI",
+      "It is a volunteer management system developed using reactjs.",
+    url: "https://volun.netlify.app/",
   },
 ];
 
